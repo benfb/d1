08749148d8f8db55d44f9dc75a1b31a568f15caf
Ben Bailey <bennettbailey@gmail.com>
2014-07-10 16:54:07 -0500

    implement email method and begin to implement sendgrid

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -13,11 +13,29 @@ angular.module('d1.controllers', [])
       console.log($scope.selected[i].phone);
     }
   };
-  $scope.sendEmail = function(){};
+  $scope.sendEmail = function(){
+    $scope.getSelected();
+    for(var i = 0; i < $scope.selected.length; i++){
+      var sendgrid  = require('sendgrid')(api_user, api_key);
+      sendgrid.send({
+        to:       $scope.selected[i].email,
+        from:     'other@example.com',
+        subject:  'Emergency! I need your help',
+        text:     'My first email through SendGrid.'
+      }, function(err, json) {
+        if (err) { return console.error(err); }
+        console.log(json);
+      });
+      console.log($scope.selected[i].email);
+    }
+  };
 })
 
 .controller('CrisisCtrl', function($scope, Contacts) {
   $scope.contacts = Contacts.all();
+  $scope.sendCrisis = function() {
+
+  }
 })
 
 .controller('ContactsCtrl', function($scope, Contacts) {
