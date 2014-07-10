8fd3a3d6fcd7f2a05d9f05ac36c15854f1c9f185
Ben Bailey <bennettbailey@gmail.com>
2014-07-10 16:45:32 -0500

    sendText() successfully logs phone numbers

--- platforms/ios/www/js/controllers.js
+++ platforms/ios/www/js/controllers.js
@@ -2,8 +2,18 @@ angular.module('d1.controllers', [])
 
 .controller('AlertCtrl', function($scope, Contacts) {
   $scope.contacts = Contacts.all();
-  $scope.selected = Contacts.all().filter(function(x){ return x.selected; })
-  $scope.logSelected = function() {console.log(Contacts.all().filter(function(x){ return x.selected; }));}
+  $scope.getSelected = function() {$scope.selected = Contacts.all().filter(function(x){ return x.selected; });}
+  $scope.logSelected = function() {
+    $scope.getSelected();
+    console.log($scope.selected);
+  }
+  $scope.sendText = function(){
+    $scope.getSelected();
+    for(var i = 0; i < $scope.selected.length; i++){
+      console.log($scope.selected[i].phone);
+    }
+  };
+  $scope.sendEmail = function(){};
 })
 
 .controller('CrisisCtrl', function($scope, Contacts) {
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,9 +2,17 @@ angular.module('d1.controllers', [])
 
 .controller('AlertCtrl', function($scope, Contacts) {
   $scope.contacts = Contacts.all();
-  $scope.selected = Contacts.all().filter(function(x){ return x.selected; })
-  $scope.logSelected = function() {console.log(Contacts.all().filter(function(x){ return x.selected; }));}
-  $scope.sendText = function(){};
+  $scope.getSelected = function() {$scope.selected = Contacts.all().filter(function(x){ return x.selected; });}
+  $scope.logSelected = function() {
+    $scope.getSelected();
+    console.log($scope.selected);
+  }
+  $scope.sendText = function(){
+    $scope.getSelected();
+    for(var i = 0; i < $scope.selected.length; i++){
+      console.log($scope.selected[i].phone);
+    }
+  };
   $scope.sendEmail = function(){};
 })
 
